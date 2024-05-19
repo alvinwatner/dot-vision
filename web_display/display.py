0f0530074e16655ab67966688bc624ccbeeeb66c
alvinwatner <alvinsetiadi22@gmail.com>
2024-05-19 13:38:34 +0800

    fix: NoneType on transformed_points uniterateable on data API

--- web_display/display.py
+++ web_display/display.py
@@ -22,22 +22,35 @@ def get_position():
     # Fetch width and height from query parameters
     w_prime = request.args.get('width', default=1000, type=int)
     h_prime = request.args.get('height', default=1000, type=int)
+    
     print(f"ensemble_model.image2d = {ensemble_model.image2d.shape}")
+    
     h = ensemble_model.image2d.shape[0]
     w = ensemble_model.image2d.shape[1]
     h_scalar = h_prime / h
     w_scalar = w_prime / w
+    
     transformed_points = ensemble_model()
     
-    print(f"h_scalar = {h_scalar} w_scalar = {w_scalar} w = {w} h = {h} w_prime = {w_prime} h_prime {h_prime}")
-    points = [scale(point.flatten().astype(int).tolist(), w_scalar, h_scalar) for point in transformed_points]
-    print(f"points = {points}")
+    print(f"h_scalar = {h_scalar} w_scalar = {w_scalar} w = {w} h = {h} w_prime = {w_prime} h_prime = {h_prime}")
+    print(f"transformed_points = {transformed_points}")
+    
+    if transformed_points is None:
+        return jsonify(error="transformed_points is None"), 500
+    
+    try:
+        points = [scale(point.flatten().astype(int).tolist(), w_scalar, h_scalar) for point in transformed_points]
+        print(f"points = {points}")
+    except Exception as e:
+        print(f"error = {e}")
+        return jsonify(error=str(e)), 500
 
-    print(f"transformed_points = {transformed_points} width = {w_prime} height = {h_prime}")
     position = {
         "points": points,
     }
+    
     return jsonify(position=position)
+
 
 
 def scale(point, w_scalar, h_scalar):
