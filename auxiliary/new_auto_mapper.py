2786429e3f3ee8e78bda5b681d570d650b7b3acf
kaorikizuna <34364395+Agung-Krisna@users.noreply.github.com>
2024-08-04 18:16:39 +0800

    feat: added checking and ensuring that the transformed points remain functional

--- auxiliary/new_auto_mapper.py
+++ auxiliary/new_auto_mapper.py
@@ -14,19 +14,24 @@ class AutoMapper:
                  **kwargs):
         model_interpreter = ModelInterpreter(model_path, threshold, accelerator, labels,
                                              kwargs.get("frame_interval", 24))
-        image_handler = ImageHandler(image2Ddir, image3Ddir)
+        self.image_handler = ImageHandler(image2Ddir, image3Ddir)
         self.homographic_handler = HomographicHandler(coors2Ddir, coors3Ddir)
         self.cv2_handler = CV2Handler(cap=cap,
-                                      image_handler=image_handler,
+                                      image_handler=self.image_handler,
                                       homographic_handler=self.homographic_handler,
                                       model_interpreter=model_interpreter,
                                       is_save=kwargs.get("is_save", False),
                                       output_file=kwargs.get("output_file", "output_result.mp4"))
 
     def generate_raw_outputs(self):
-        # return transformed coordinates from each tracker available
-        return [self.homographic_handler.transform_coordinates(tracker.bottom_center) for tracker in
-                self.cv2_handler.model_interpreter.tracking_handler.trackers]
+        """
+        Reading the frame, performing detection and tracking and then returning transformed coordinates
+        """
+        # if frame can be read, perform detection, then return
+        if self.cv2_handler.read_capture():
+            self.cv2_handler.model_interpreter.detect_and_track_objects(self.cv2_handler.frame_dataclass)
+            return [self.homographic_handler.transform_coordinates(tracker.bottom_center) for tracker in
+                    self.cv2_handler.model_interpreter.tracking_handler.trackers]
 
     def stream_using_cv2(self):
         self.cv2_handler.process_frame()
