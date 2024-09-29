23329b7a7f2bce61d58600527680b6a5d6420e54
kaorikizuna <34364395+Agung-Krisna@users.noreply.github.com>
2024-09-30 03:13:51 +0800

    feat: configuration for the app in simple python

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import logging
+from pathlib import Path
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+
+CONFIDENCE_THRESHOLD = 0.5
